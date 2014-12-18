f777558e83c66e478c3ec92d66ad765e291ae25c
Yu Hao <yuhao89@live.cn>
2014-12-18 16:26:27 +0800

    add zoeppritz

--- test_raytracing.py
+++ test_raytracing.py
@@ -4,28 +4,35 @@ Created on Fri Dec 12 15:45:54 2014
 
 @author: yuhao
 """
-from raytrace import *
-import raytraceClass as rc
+#from raytrace import *
+#import raytraceClass as rc
 import numpy as np
-
+import zoeppritz as z
 #vp = [1500, 2000, 2500, 3000, 3500]
-vp = [1500, 2000]
+#3vp = [1500, 2000]
 #vs = [1500, 1600, 2000, 2250]
 #rho = [2.3, 2.7, 2.8, 3.0]
 #thic = [50, 50, 50, 50]
-thic = [50]
-offset = [25, 50, 75, 100, 125]
+#thic = [50]
+#offset = [25, 50, 75, 100, 125]
 #offset = [50, 100, 150, 200, 250]
 #offset = np.arange(1000, 3000, 100)
 #zp=[200, 100, 150, 300, 250, 700, 800, 1000, 500]
 #vp=[1500, 1600, 2000, 2250, 2700, 2100, 3200, 3750, 4000, 4200]
-ppp = raytrace(vp, thic, offset)
+#ppp = raytrace(vp, thic, offset)
 
-a = rc.Raytrace()
-a.offset = offset
-a.Vp = vp
-print a.Vp
-a.thic = thic
-a.raytrace()
-print a.pm
-kkk = a.pm
+#a = rc.Raytrace()
+#a.offset = offset
+#a.Vp = vp
+#a.thic = thic
+#a.raytrace()
+#kkk = a.pm
+vp1 = 1500
+vp2 = 2000
+vs1 = 1000
+vs2 = 1300
+rho1 = 1300
+rho2 = 1400
+p = np.sin(np.pi / 6.) / 1500.
+#p = np.sin(np.pi / 18.) / 1500.
+a, b = z.zoeppritz(vp1, vp2, vs1, vs2, rho1, rho2, p)
---
+++ zoeppritz.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 17 14:53:12 2014
+
+@author: yuhao
+"""
+
+def zoeppritz(vp1, vp2, vs1, vs2, rho1, rho2, p):
+    q = 2 * (rho2 * vs2**2 - rho1 * vs1**2)
+    X = rho2 - q * p**2
+    Y = rho1 + q * p**2
+    Z = rho2 - rho1 - q * p**2
+    P1 = (1 - vp1**2 * p**2)**0.5
+    P2 = (1 - vs1**2 * p**2)**0.5
+    P3 = (1 - vp2**2 * p**2)**0.5
+    P4 = (1 - vs2**2 * p**2)**0.5
+    
+    D = q**2 * p**2 * P1 * P2 * P3 * P4 + \
+        rho1 * rho2 *(vs1*vp2*P1*P4 + vp1*vs2*P2*P3) + \
+        vp1 * vs1 * P3 * P4 * Y**2 + \
+        vp2 * vs2 * P1 * P2 * X**2 + \
+        vp1 * vp2 * vs1 * vs2 * p**2 * Z**2
+    
+    
+    Nr = q**2 * p**2 * P1 * P2 * P3 * P4 + \
+        rho1 * rho2 *(vs1*vp2*P1*P4 - vp1*vs2*P2*P3) - \
+        vp1 * vs1 * P3 * P4 * Y**2 + \
+        vp2 * vs2 * P1 * P2 * X**2 - \
+        vp1 * vp2 * vs1 * vs2 * p**2 * Z**2
+        
+    Nt = 2 * vp1 * rho1 * P1 * (vs2 * P2 * X + vs1 * P4 * Y)
+    
+    rpp = Nr / D
+    
+    tpp = Nt / D
+    
+    return rpp, tpp

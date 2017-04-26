f3fe3e83c26d29c021f608f9aa057e37277318d7
Yu Hao <yuhao89@live.cn>
2017-04-26 19:14:05 +0800

    add comments, and refactor variable names

--- explorationpy/raytrace.py
+++ explorationpy/raytrace.py
@@ -1,59 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+Created in 2014
+Modifed on Apr. 26th 2017
+"""
+from __future__ import absolute_import, division, print_function
+
+__author__ = "yuhao"
+
 import numpy as np
 
 
-def raytrace(Vp, thic, offset):
+def raytrace(velocity, thickness, offset):
+    """
+    Raytracing for simple 1-D VTI model.
 
-    pm = np.zeros((len(thic), len(offset)))
-    tm = np.zeros((len(thic), len(offset)))
-    for ii in xrange(len(thic)):  # for each interface
-        for io in xrange(len(offset)):  # for each offset
+    Calculate reflection coefficient and travel time for each interface given
+    layer thickness and offset distances.
+
+    Parameters
+    ----------
+    velocity : 1-d ndarray
+        Velocity of each layer
+
+    thickness : 1-d ndarray
+        Thickness of each layer
+
+    offset : 1-d ndarray
+        Offset positions to be calculated in horizontal direction
+
+    Return
+    ------
+    pm : 2-d ndarray
+        Reflection coefficients for each layer and each offset
+
+    tm : 2-d ndarray
+        Travel time for each layer and each offset
+    """
+    m = len(thickness)
+    n = len(offset)
+    pm = np.zeros((m, n))
+    tm = np.zeros((m, n))
+    for ii in xrange(m):  # for each interface
+        for io in xrange(n):  # for each offset
             err = offset[io]
             counter = 0
-            p0 = np.sin(np.pi/4) / Vp[ii]
+            p0 = np.sin(np.pi/4) / velocity[ii]
             flag = False
             while err > 0.01 * offset[io]:
+                # deviation from target using p0
                 y0 = 0
                 for i in xrange(ii + 1):
-                    y0 += (thic[i] * Vp[i] * p0) / np.sqrt(1 - Vp[i]**2 *
-                                                           p0**2)
+                    y0 += (thickness[i] * velocity[i] * p0) / np.sqrt(1 - velocity[i]**2 * p0**2)
                 y0 = 2 * y0
-
                 ydelta = offset[io] - y0
-
+                # correct p0
                 pg = 0  # gradient of p relate to y
                 for i in range(ii + 1):
-                    pg += (thic[i] * Vp[i]) / ((1 - Vp[i]**2 * p0**2)**(1.5))
+                    pg += (thickness[i] * velocity[i]) / ((1 - velocity[i]**2 * p0**2)**(1.5))
                 pg = pg**(-1)
                 pg = 0.5 * pg
-
                 p0 += pg * ydelta  # corrected p
-
+                # calculate error for new p0
                 y = 0
                 for i in range(ii + 1):
-                    y += (thic[i] * Vp[i] * p0) / np.sqrt(1 - Vp[i]**2 * p0**2)
+                    y += (thickness[i] * velocity[i] * p0) / np.sqrt(1 - velocity[i]**2 * p0**2)
                 y = 2 * y
-
                 err = np.abs(y - offset[io])
-
+                # break when correct for 100 times
                 counter += 1
                 if counter == 100:
-                    flag = True
+                    p0 = np.nan
                     break
-            if flag is True:
-                pm[ii, io] = np.NaN
-            else:
-                pm[ii, io] = p0
-
-    for ii in xrange(len(thic)):  # for each interface
-        for io in xrange(len(offset)):  # for each offset
+            pm[ii, io] = p0
+    # Calculate Travel time along specific trace
+    for ii in xrange(m):  # for each interface
+        for io in xrange(n):  # for each offset
             traveltime = 0
             p = pm[ii, io]
             for i in range(ii + 1):
-                traveltime = traveltime + 2 * thic[i] /\
-                             (Vp[i] * np.sqrt(1 - Vp[i]**2 * p**2))
+                traveltime = traveltime + 2 * thickness[i] /\
+                             (velocity[i] * np.sqrt(1 - velocity[i]**2 * p**2))
             tm[ii, io] = traveltime
 
     return pm, tm
-
-if __name__ == "__main__":
-    pass
